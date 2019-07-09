602f5c97c1d0ad36d4e025fcc6df2084ecfe07ed
Forrest Hilton <forrestmhilton@gmail.com>
2019-07-08 21:11:26 -0500

    Initial Commit

---
+++ DrawShape/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DrawShape
+//
+//  Created by Forrest Hilton on 7/8/19.
+//  Copyright © 2019 Forrest Hilton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
